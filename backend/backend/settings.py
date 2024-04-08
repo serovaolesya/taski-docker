b6834d11407f88b42206935b71a8c41998fe77cf
Олеся Серова <olesia-serova@yandex.ru>
2024-04-08 05:47:46 +0300

    Change domain.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.160.255', '127.0.0.1', 'localhost', 'justdoit.webhop.me']
+ALLOWED_HOSTS = ['84.201.160.255', '127.0.0.1', 'localhost', 'thatsmypet.redirectme.net']
 
 
 # Application definition
